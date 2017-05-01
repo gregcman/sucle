a0fc230a9ed052130c989c9945774917673b5dcb
terminal625 <gregoriomanabat@gmail.com>
2017-05-01 02:43:17 -0400

    my offsets have offsets!!
    -symbols bound in incremental order
    -sub levels are offsets of offsets ...
    -to bind a vector like a struct, splice compile-time constants into the offset vectors, to bind it like an object, use runtime values

--- funland/ultra/with-vec-params.lisp
+++ funland/ultra/with-vec-params.lisp
@@ -2,35 +2,57 @@
 (export (quote with-vec-params))
 (defmacro with-vec-params ((&rest bufvars) (buf) &body body)
   (let ((letargs nil)
-	(ans (%aux-with-vec-params bufvars buf)))
-    (labels ((rec-push (stuff)
-	     (dolist (x stuff)
-	       (let ((first (car x)))
-		 (if (atom first)
-		     (push x letargs)
-		     (rec-push x))))))
-      (rec-push ans))
-    `(let ,letargs 
-       ,@body)))
+	(letargsoffset nil)
+	(decl nil))
+    (multiple-value-bind (ans offs) (%aux-with-vec-params bufvars buf)
+      (labels ((rec-push (stuff)
+		 (dolist (x stuff)
+		   (let ((first (car x)))
+		     (if (consp first)
+			 (rec-push x)
+			 (push x letargs))))))
+	(rec-push ans))
+      (labels ((rec-push-offs (stuff)
+		 (dolist (x stuff)
+		   (let ((first (car x)))
+		     (if (consp first)			 
+			 (rec-push-offs x)
+			 (when first (push x letargsoffset)
+			       (push (car x) decl)))))))
+	(rec-push-offs offs)))
+    (let ((fin `(let ,letargs 
+		  ,@body)))
+      (if letargsoffset
+	  `(let* ,letargsoffset
+	     (declare (type fixnum ,@decl))
+	     ,fin)
+	  fin))))
 
 (defun %aux-with-vec-params (vars-or-offsets buf &optional (offset 0))
   (let ((counter 0)
-	(acc nil))
+	(bindings nil)
+	(offsets nil))
     (dolist (var-or-offset vars-or-offsets)
       (if (consp var-or-offset)
 	  (let ((suboffset (car var-or-offset)))
-	    (push (%aux-with-vec-params
-		   (cdr var-or-offset)
-		   buf
-		   (if (zerop offset)
-		       suboffset
-		       `(the fixnum (+ ,offset ,suboffset))))
-		  acc))
+	    (multiple-value-bind (bindings-list offsets-list)
+		(%aux-with-vec-params
+		 (cdr var-or-offset)
+		 buf
+		 (if (eql 0 offset)
+		     suboffset
+		     (let ((newoffset (gensym)))
+		       (push `(,newoffset (the fixnum (+ ,offset ,suboffset))) offsets)
+		       newoffset)))
+	      (when bindings-list
+		(push bindings-list bindings))
+	      (when offsets-list
+		(push offsets-list offsets))))
 	  (progn
-	    (let ((sub (if (= 0 counter)
+	    (let ((sub (if (eql 0 counter)
 			   offset
 			   `(the fixnum (+ ,offset ,counter)))))
 	      (when var-or-offset
-		(push `(,var-or-offset (aref ,buf ,sub)) acc)))
+		(push `(,var-or-offset (aref ,buf ,sub)) bindings)))
 	    (incf counter))))
-    acc))
+    (values bindings offsets)))
