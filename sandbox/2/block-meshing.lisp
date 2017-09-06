749189add73463ccdd46b26cf2029a3dd1db7c42
terminal625 <zpersoncool@gmail.com>
2017-09-06 09:11:56 -0400

    block meshing?

--- sandbox/2/block-meshing.lisp
+++ sandbox/2/block-meshing.lisp
@@ -1,49 +1,37 @@
 (in-package :sandbox)
 
-(defparameter shapebuffer-index 0)
-(defparameter shapebuffer-vs (make-array (ash 2 20) :element-type 'single-float))
 
-(declaim (inline add-to-shapebuffer))
-(defun add-to-shapebuffer (x y z u v darkness b0 b1 b2 b3 s0 s1 s2 s3)
-  (declare (type (simple-array single-float *) shapebuffer-vs))
-  (declare (type single-float x y z darkness))
-  (declare (type (unsigned-byte 4) b0 b1 b2 b3 s0 s1 s2 s3))
-					;(declare (optimize (speed 3) (safety 0)))
-  (declare (ignorable b0 b1 b2 b3))
-  (let ((vertlength shapebuffer-index))
-    (declare (type fixnum vertlength))
-    (let ((fp (* 6 vertlength)))
-      (setf (aref shapebuffer-vs (+ fp 0)) x
-	    (aref shapebuffer-vs (+ fp 1)) y
-	    (aref shapebuffer-vs (+ fp 2)) z
-	    (aref shapebuffer-vs (+ fp 3)) (coerce u 'single-float)
-	    (aref shapebuffer-vs (+ fp 4)) (coerce v 'single-float)	    
-	    (aref shapebuffer-vs (+ fp 5))
-	    (* darkness
-	       0.25
-	       (+ (max b0 (* daytime s0))
-		  (max b1 (* daytime s1))
-		  (max b2 (* daytime s2))
-		  (max b3 (* daytime s3)))))))
-  (incf shapebuffer-index))
 
-(defun chunk-shape (chunk-position)
-  (declare (optimize (speed 3)))
-  (multiple-value-bind (io ko jo) (world:unhashfunc chunk-position)
-    (setf shapebuffer-index 0)
-    (let ((new-shape shapebuffer-vs))
-      (dobox ((i io (+ 16 io))
-	      (j jo (+ 16 jo))
-	      (k ko (+ 16 ko)))
-	     (let ((blockid (world:getblock i j k)))
-	       (unless (zerop blockid)
-		 (blockshape
-		  i j k
-		  blockid))))
-      (values
-       new-shape
-       shapebuffer-index
-       chunk-position))))
+(defun dark-fun (darkness b0 b1 b2 b3 s0 s1 s2 s3)
+  (let ((time daytime))
+    (* darkness
+       0.25
+       (+ (max b0 (* time s0))
+	  (max b1 (* time s1))
+	  (max b2 (* time s2))
+	  (max b3 (* time s3))))))
+
+(defparameter *bluffs* nil)
+
+(defun chunk-shape (chunk-position iter)
+  (declare (optimize (debug 3)))
+  (let ((buf (aplayground::get-buf-param iter
+					 (etouq (vector 0 2 8)))))
+    (aplayground::reset-attrib-buffer-iterators iter)
+    (let ((times 0))
+      (let ((*bluffs* buf))
+	(multiple-value-bind (io ko jo) (world:unhashfunc chunk-position)
+	  (dobox ((i io (+ 16 io))
+		  (j jo (+ 16 jo))
+		  (k ko (+ 16 ko)))
+		 (let ((blockid (world:getblock i j k)))
+		   (unless (zerop blockid)
+		     (incf times
+			   (blockshape
+			    i j k
+			    blockid)))))))
+      (values buf times chunk-position iter))))
+
 
 (defun blockshape (i j k blockid)
   (case blockid
@@ -66,49 +54,66 @@
 	   (let ((,y-name (- 15 (ash (- ,num-form-sym ,x-name) -4))))
 	     ,@body))))))
 
+
 (defun rendergrass (id i j k)
-  (with-texture-translator (tu tv) 2
-    (let ((adj-id (world:getblock i (1- j) k)))
-      (when (show-sidep id adj-id) 
-	(side-j i j k tu tv))))
-  (with-texture-translator (tu tv) 0
-    (let ((adj-id (world:getblock i (1+ j) k)))
-      (when (show-sidep id adj-id) 
-	(side+j i j k tu tv))))
-  (with-texture-translator (tu tv) 3
-    (let ((adj-id (world:getblock (1- i) j k)))
-      (when (show-sidep id adj-id) 
-	(side-i i j k tu tv)))
-    (let ((adj-id (world:getblock (1+ i) j k)))
-      (when (show-sidep id adj-id) 
-	(side+i i j k tu tv)))    
-    (let ((adj-id (world:getblock i j (1- k))))
-      (when (show-sidep id adj-id)
-	(side-k i j k tu tv)))
-    (let ((adj-id (world:getblock i j (1+ k))))
-      (when (show-sidep id adj-id) 
-	(side+k i j k tu tv)))))
+  (let ((times 0))
+    (with-texture-translator (tu tv) 2
+      (let ((adj-id (world:getblock i (1- j) k)))
+	(when (show-sidep id adj-id)
+	  (incf times 4)
+	  (side-j i j k tu tv))))
+    (with-texture-translator (tu tv) 0
+      (let ((adj-id (world:getblock i (1+ j) k)))
+	(when (show-sidep id adj-id)
+	  (incf times 4)
+	  (side+j i j k tu tv))))
+    (with-texture-translator (tu tv) 3
+      (let ((adj-id (world:getblock (1- i) j k)))
+	(when (show-sidep id adj-id)
+	  (incf times 4)
+	  (side-i i j k tu tv)))
+      (let ((adj-id (world:getblock (1+ i) j k)))
+	(when (show-sidep id adj-id)
+	  (incf times 4)
+	  (side+i i j k tu tv)))    
+      (let ((adj-id (world:getblock i j (1- k))))
+	(when (show-sidep id adj-id)
+	  (incf times 4)
+	  (side-k i j k tu tv)))
+      (let ((adj-id (world:getblock i j (1+ k))))
+	(when (show-sidep id adj-id)
+	  (incf times 4)
+	  (side+k i j k tu tv))))
+    times))
 
 (defun renderstandardblock (id i j k)
-  (with-texture-translator (tu tv) (aref mc-blocks::blockIndexInTexture id)
-    (let ((adj-id (world:getblock i (1- j) k)))
-      (when (show-sidep id adj-id) 
-	(side-j i j k tu tv)))
-    (let ((adj-id (world:getblock i (1+ j) k)))
-      (when (show-sidep id adj-id) 
-	(side+j i j k tu tv)))
-    (let ((adj-id (world:getblock (1- i) j k)))
-      (when (show-sidep id adj-id) 
-	(side-i i j k tu tv)))
-    (let ((adj-id (world:getblock (1+ i) j k)))
-      (when (show-sidep id adj-id) 
-	(side+i i j k tu tv)))    
-    (let ((adj-id (world:getblock i j (1- k))))
-      (when (show-sidep id adj-id)
-	(side-k i j k tu tv)))
-    (let ((adj-id (world:getblock i j (1+ k))))
-      (when (show-sidep id adj-id) 
-	(side+k i j k tu tv)))))
+  (let ((times 0))
+    (with-texture-translator (tu tv) (aref mc-blocks::blockIndexInTexture id)
+      (let ((adj-id (world:getblock i (1- j) k)))
+	(when (show-sidep id adj-id)
+	  (incf times 1)
+	  (side-j i j k tu tv)))
+      (let ((adj-id (world:getblock i (1+ j) k)))
+	(when (show-sidep id adj-id)
+	  (incf times 1)
+	  (side+j i j k tu tv)))
+      (let ((adj-id (world:getblock (1- i) j k)))
+	(when (show-sidep id adj-id)
+	  (incf times 1)
+	  (side-i i j k tu tv)))
+      (let ((adj-id (world:getblock (1+ i) j k)))
+	(when (show-sidep id adj-id)
+	  (incf times 1)
+	  (side+i i j k tu tv)))    
+      (let ((adj-id (world:getblock i j (1- k))))
+	(when (show-sidep id adj-id)
+	  (incf times 1)
+	  (side-k i j k tu tv)))
+      (let ((adj-id (world:getblock i j (1+ k))))
+	(when (show-sidep id adj-id)
+	  (incf times 1)
+	  (side+k i j k tu tv))))
+    (* 4 times)))
 
 (defmacro %edge-aux ((i j k)
 		     getfunc
@@ -217,133 +222,303 @@
 (defun trans (foo foo-translator)
   (/ (+ foo foo-translator) 16.0))
 
+(defmacro texface ((u0 v0)
+		   (u1 v1)
+		   (u2 v2)
+		   (u3 v3))
+  `(progn    
+     (etex (coerce (trans ,u0 u-offset) 'single-float))
+     (etex (coerce (trans ,v0 v-offset) 'single-float))     
+     (etex (coerce (trans ,u1 u-offset) 'single-float))
+     (etex (coerce (trans ,v1 v-offset) 'single-float))    
+     (etex (coerce (trans ,u2 u-offset) 'single-float))
+     (etex (coerce (trans ,v2 v-offset) 'single-float))   
+     (etex (coerce (trans ,u3 u-offset) 'single-float))
+     (etex (coerce (trans ,v3 v-offset) 'single-float))))
+
 (defmacro squareface (light-edge-fnc
 		      skylight-edge-fnc
-		      color
-		      (x0 y0 z0)
-		      (u0 v0)
-		      (i0 j0 k0)
-		      (x1 y1 z1)
-		      (u1 v1)
-		      (i1 j1 k1)
-		      (x2 y2 z2)
-		      (u2 v2)
-		      (i2 j2 k2)
-		      (x3 y3 z3)
-		      (u3 v3)
+		      color	      
+		      (i0 j0 k0)		      
+		      (i1 j1 k1)		      
+		      (i2 j2 k2)		      
 		      (i3 j3 k3))
+  (let ((light-edge-code (funcall (symbol-function light-edge-fnc) 'xpos 'ypos 'zpos))
+	(sky-edge-code (funcall (symbol-function skylight-edge-fnc) 'xpos 'ypos 'zpos)))
+    `(progn
+      (let ((xpos (+ i ,i0))
+	    (ypos (+ j ,j0))
+	    (zpos (+ k ,k0))
+
+	    )
+	(declare (type fixnum xpos ypos zpos))
+
+	(dark (dark-fun
+	       ,color
+	       ,@light-edge-code
+	       ,@sky-edge-code)))
+      (let ((xpos (+ i ,i1))
+	    (ypos (+ j ,j1))
+	    (zpos (+ k ,k1))
+
+	    )
+	(declare (type fixnum xpos ypos zpos ))
+
+	(dark (dark-fun
+	       ,color
+	       ,@light-edge-code
+	       ,@sky-edge-code)))
+      (let ((xpos (+ i ,i2))
+	    (ypos (+ j ,j2))
+	    (zpos (+ k ,k2))
+
+	    )
+	(declare (type fixnum xpos ypos zpos))
+
+	(dark (dark-fun
+	       ,color
+	       ,@light-edge-code
+	       ,@sky-edge-code)))
+
+      
+      (let ((xpos (+ i ,i3))
+	    (ypos (+ j ,j3))
+	    (zpos (+ k ,k3))
+
+	    )
+	(declare (type fixnum xpos ypos zpos ))
+
+	(dark (dark-fun
+	       ,color
+	       ,@light-edge-code
+	       ,@sky-edge-code))))
+    
+    ))))
+
+(defmacro posface ((x0 y0 z0) 
+		   (x1 y1 z1)		      
+		   (x2 y2 z2)		     
+		   (x3 y3 z3)) 
   `(progn
-     (let ((xpos (+ i ,i0))
-	   (ypos (+ j ,j0))
-	   (zpos (+ k ,k0))
+     (let (
 	   (xp (+ i ,x0))
 	   (yp (+ j ,y0))
 	   (zp (+ k ,z0)))
-       (declare (type fixnum xpos ypos zpos xp yp zp))
-       (add-to-shapebuffer
-	(coerce xp 'single-float) (coerce yp 'single-float) (coerce zp 'single-float) 
-	(trans ,u0 u-offset) (trans ,v0 v-offset)
-	,color
-	,@(funcall (symbol-function light-edge-fnc) 'xpos 'ypos 'zpos)
-	,@(funcall (symbol-function skylight-edge-fnc) 'xpos 'ypos 'zpos)))
-     (let ((xpos (+ i ,i1))
-	   (ypos (+ j ,j1))
-	   (zpos (+ k ,k1))
+       (declare (type fixnum xp yp zp))
+       
+       (epos (coerce xp 'single-float))
+       (epos (coerce yp 'single-float))
+       (epos (coerce zp 'single-float)) 
+       )
+     (let (
 	   (xp (+ i ,x1))
 	   (yp (+ j ,y1))
 	   (zp (+ k ,z1)))
-       (declare (type fixnum xpos ypos zpos xp yp zp))
-       (add-to-shapebuffer
-	(coerce xp 'single-float) (coerce yp 'single-float) (coerce zp 'single-float) 
-	(trans ,u1 u-offset) (trans ,v1 v-offset)
-	,color
-	,@(funcall (symbol-function light-edge-fnc) 'xpos 'ypos 'zpos)
-	,@(funcall (symbol-function skylight-edge-fnc) 'xpos 'ypos 'zpos)))
-     (let ((xpos (+ i ,i2))
-	   (ypos (+ j ,j2))
-	   (zpos (+ k ,k2))
-	   (xp (+ i ,x2))
+       (declare (type fixnum  xp yp zp))
+       
+       (epos (coerce xp 'single-float))
+       (epos (coerce yp 'single-float))
+       (epos (coerce zp 'single-float)) 
+       )
+     (let ((xp (+ i ,x2))
 	   (yp (+ j ,y2))
 	   (zp (+ k ,z2)))
-       (declare (type fixnum xpos ypos zpos xp yp zp))
-       (add-to-shapebuffer
-	(coerce xp 'single-float) (coerce yp 'single-float) (coerce zp 'single-float) 
-	(trans ,u2 u-offset) (trans ,v2 v-offset)
-	,color
-	,@(funcall (symbol-function light-edge-fnc) 'xpos 'ypos 'zpos)
-	,@(funcall (symbol-function skylight-edge-fnc) 'xpos 'ypos 'zpos)))
-     (let ((xpos (+ i ,i3))
-	   (ypos (+ j ,j3))
-	   (zpos (+ k ,k3))
+       (declare (type fixnum  xp yp zp))
+       (epos (coerce xp 'single-float))
+       (epos (coerce yp 'single-float))
+       (epos (coerce zp 'single-float)) 
+       ) 
+     (let (
 	   (xp (+ i ,x3))
 	   (yp (+ j ,y3))
 	   (zp (+ k ,z3)))
-       (declare (type fixnum xpos ypos zpos xp yp zp))
-       (add-to-shapebuffer
-	(coerce xp 'single-float) (coerce yp 'single-float) (coerce zp 'single-float) 
-	(trans ,u3 u-offset) (trans ,v3 v-offset)
-	,color
-	,@(funcall (symbol-function light-edge-fnc) 'xpos 'ypos 'zpos)
-	,@(funcall (symbol-function skylight-edge-fnc) 'xpos 'ypos 'zpos)))))
+       (declare (type fixnum  xp yp zp))
+       (epos (coerce xp 'single-float))
+       (epos (coerce yp 'single-float))
+       (epos (coerce zp 'single-float)) )))
 
-(progn
-  (defun side-i (i j k u-offset v-offset)
-    (declare (type fixnum i j k u-offset v-offset))
-    (declare (optimize (speed 3) (safety 0)))
-    (squareface light-edge-i
-		skylight-edge-i
-		0.6
-		(0 0 0) (0 0) (-1 -1 -1)
-		(0 0 1) (1 0) (-1 -1 00)
-		(0 1 1) (1 1) (-1 00 00)
-		(0 1 0) (0 1) (-1 00 -1)))
-  (defun side+i (i j k u-offset v-offset)
-    (declare (type fixnum i j k u-offset v-offset))
-    (declare (optimize (speed 3) (safety 0)))
-    (squareface light-edge-i
-		skylight-edge-i
-		0.6
-		(1 0 0) (1 0) (1 -1 -1)
-		(1 1 0) (1 1) (1 00 -1)
-		(1 1 1) (0 1) (1 00 00)
-		(1 0 1) (0 0) (1 -1 00)))
-  (defun side-j (i j k u-offset v-offset)
-    (declare (type fixnum i j k u-offset v-offset))
-    (declare (optimize (speed 3) (safety 0)))
-    (squareface light-edge-j
-		skylight-edge-j
-		0.5
-		(0 0 0) (0 0) (-1 -1 -1)
-		(1 0 0) (1 0) (00 -1 -1)
-		(1 0 1) (1 1) (00 -1 00)
-		(0 0 1) (0 1) (-1 -1 00)))
-  (defun side+j (i j k u-offset v-offset)
-    (declare (type fixnum i j k u-offset v-offset))
-    (declare (optimize (speed 3) (safety 0)))
-    (squareface light-edge-j
-		skylight-edge-j
-		1.0
-		(0 1 0) (0 0) (-1 1 -1)
-		(0 1 1) (1 0) (-1 1 00)
-		(1 1 1) (1 1) (00 1 00)
-		(1 1 0) (0 1) (00 1 -1)))
-  (defun side-k (i j k u-offset v-offset)
-    (declare (type fixnum i j k u-offset v-offset))
-    (declare (optimize (speed 3) (safety 0)))
-    (squareface light-edge-k
-		skylight-edge-k
-		0.8   
-		(0 0 0) (1 0) (-1 -1 -1)
-		(0 1 0) (1 1) (-1 00 -1)
-		(1 1 0) (0 1) (00 00 -1)
-		(1 0 0) (0 0) (00 -1 -1)))
-  (defun side+k (i j k u-offset v-offset)
-    (declare (type fixnum i j k u-offset v-offset))
-    (declare (optimize (speed 3) (safety 0)))
-    (squareface light-edge-k
-		skylight-edge-k
-		0.8     
-		(0 0 1) (0 0) (-1 -1 1)
-		(1 0 1) (1 0) (00 -1 1)    
-		(1 1 1) (1 1) (00 00 1)    
-		(0 1 1) (0 1) (-1 00 1))))
+
+
+(with-unsafe-speed
+  (progn
+    (defun side-i (i j k u-offset v-offset)
+      (declare (type fixnum i j k u-offset v-offset))
+      (aplayground::with-iterators (epos etex dark)
+	  *bluffs* iter-ator:wasabios iter-ator:iter-ator
+	(posface (0 0 0)
+		 (0 0 1)
+		 (0 1 1)
+		 (0 1 0))
+	(texface (0 0)
+		 (1 0)
+		 (1 1)
+		 (0 1))
+	(squareface light-edge-i
+		    skylight-edge-i
+		    0.6
+		    (-1 -1 -1)
+		    (-1 -1 00)
+		    (-1 00 00)
+		    (-1 00 -1))))
+    (defun side+i (i j k u-offset v-offset)
+      (declare (type fixnum i j k u-offset v-offset))
+      (aplayground::with-iterators (epos etex dark)
+	  *bluffs* iter-ator:wasabios iter-ator:iter-ator
+	(posface (1 0 0)
+		 (1 1 0)
+		 (1 1 1)
+		 (1 0 1))
+	(texface (1 0)
+		 (1 1)
+		 (0 1)
+		 (0 0))
+	(squareface light-edge-i
+		    skylight-edge-i
+		    0.6
+		    (1 -1 -1)
+		    (1 00 -1)
+		    (1 00 00)
+		    (1 -1 00))))
+    (defun side-j (i j k u-offset v-offset)
+      (declare (type fixnum i j k u-offset v-offset))
+      (aplayground::with-iterators (epos etex dark)
+	  *bluffs* iter-ator:wasabios iter-ator:iter-ator
+	(posface (0 0 0)
+		 (1 0 0)
+		 (1 0 1)
+		 (0 0 1))
+	(texface (0 0)
+		 (1 0)
+		 (1 1)
+		 (0 1)) 
+	(squareface light-edge-j
+		    skylight-edge-j
+		    0.5		  
+		    (-1 -1 -1)		   
+		    (00 -1 -1)		  
+		    (00 -1 00)		   
+		    (-1 -1 00))))
+    (defun side+j (i j k u-offset v-offset)
+      (declare (type fixnum i j k u-offset v-offset))
+      (aplayground::with-iterators (epos etex dark)
+	  *bluffs* iter-ator:wasabios iter-ator:iter-ator
+	(posface (0 1 0)
+		 (0 1 1)
+		 (1 1 1)
+		 (1 1 0))
+	(texface (0 0)
+		 (1 0)
+		 (1 1)
+		 (0 1) )
+	(squareface light-edge-j
+		    skylight-edge-j
+		    1.0
+		    (-1 1 -1)
+		    (-1 1 00)
+		    (00 1 00)
+		    (00 1 -1))))
+    (defun side-k (i j k u-offset v-offset)
+      (declare (type fixnum i j k u-offset v-offset))
+      (aplayground::with-iterators (epos etex dark)
+	  *bluffs* iter-ator:wasabios iter-ator:iter-ator
+	(posface (0 0 0)
+		 (0 1 0)
+		 (1 1 0)
+		 (1 0 0))
+	(texface (1 0)
+		 (1 1)
+		 (0 1)
+		 (0 0))
+	(squareface light-edge-k
+		    skylight-edge-k
+		    0.8   
+		    (-1 -1 -1)
+		    (-1 00 -1)
+		    (00 00 -1)
+		    (00 -1 -1))))
+    (defun side+k (i j k u-offset v-offset)
+      (declare (type fixnum i j k u-offset v-offset))
+      (aplayground::with-iterators (epos etex dark)
+	  *bluffs* iter-ator:wasabios iter-ator:iter-ator
+	(posface (0 0 1)
+		 (1 0 1)
+		 (1 1 1)
+		 (0 1 1))
+	(texface (0 0)
+		 (1 0)
+		 (1 1)
+		 (0 1))
+	(squareface light-edge-k
+		    skylight-edge-k
+		    0.8     
+		    (-1 -1 1)
+		    (00 -1 1)    
+		    (00 00 1)    
+		    (-1 00 1))))))
+
+;;(defparameter shapebuffer-index 0)
+;;(defparameter shapebuffer-vs (make-array (ash 2 20) :element-type 'single-float))
+
+					;(declare (optimize (speed 3) (safety 0)))
+;;  (declare (ignorable b0 b1 b2 b3))
+#+nil
+(defun add-to-shapebuffer (x y z u v darkness b0 b1 b2 b3 s0 s1 s2 s3)
+  (declare (type (simple-array single-float *) shapebuffer-vs))
+  (declare (type single-float x y z darkness))
+  (declare (type (unsigned-byte 4) b0 b1 b2 b3 s0 s1 s2 s3))
+					;(declare (optimize (speed 3) (safety 0)))
+  (declare (ignorable b0 b1 b2 b3))
+  (let ((vertlength shapebuffer-index))
+    (declare (type fixnum vertlength))
+    (let ((fp (* 6 vertlength)))
+      (setf (aref shapebuffer-vs (+ fp 0)) x
+	    (aref shapebuffer-vs (+ fp 1)) y
+	    (aref shapebuffer-vs (+ fp 2)) z
+	    (aref shapebuffer-vs (+ fp 3)) (coerce u 'single-float)
+	    (aref shapebuffer-vs (+ fp 4)) (coerce v 'single-float)	    
+	    (aref shapebuffer-vs (+ fp 5))
+	    (* darkness
+	       0.25
+	       (+ (max b0 (* daytime s0))
+		  (max b1 (* daytime s1))
+		  (max b2 (* daytime s2))
+		  (max b3 (* daytime s3)))))))
+  (incf shapebuffer-index))
+
+
+#+nil
+(defun chunk-shape (chunk-position)
+  (declare (optimize (speed 3)))
+  (multiple-value-bind (io ko jo) (world:unhashfunc chunk-position)
+    (setf shapebuffer-index 0)
+    (let ((new-shape shapebuffer-vs))
+      (dobox ((i io (+ 16 io))
+	      (j jo (+ 16 jo))
+	      (k ko (+ 16 ko)))
+	     (let ((blockid (world:getblock i j k)))
+	       (unless (zerop blockid)
+		 (blockshape
+		  i j k
+		  blockid))))
+      (values
+       new-shape
+       shapebuffer-index
+       chunk-position))))
+
+#+nil
+(progno
+ (declaim (inline add-to-shapebuffer))
+ (defun add-to-shapebuffer (x y z u v)
+   (declare (type (simple-array single-float *) shapebuffer-vs))
+   (declare (type single-float x y z darkness))
+   (declare (type (unsigned-byte 4) b0 b1 b2 b3 s0 s1 s2 s3))
+
+   (aplayground::with-iterators (epos etex dark)
+       *bluffs* iter-ator:wasabios iter-ator:iter-ator
+     (epos x)
+     (epos y)
+     (epos z)
+     (etex (coerce u 'single-float))
+     (etex (coerce v 'single-float))	    
+     (dark
+      ))))
--- sandbox/meshing-thread.lisp
+++ sandbox/meshing-thread.lisp
@@ -9,6 +9,66 @@
       (when achunk
 	(giveworktomesherthread achunk)))))
 
+(defun mesh-chunk (times bufs)
+  (aplayground::with-iterators (xyz uv dark) bufs iter-ator:wasabiis iter-ator:iter-ator
+    (dotimes (x times)
+      (%gl:vertex-attrib-1f 8 (dark))
+      (%gl:vertex-attrib-2f 2 (uv) (uv))
+      (%gl:vertex-attrib-3f 0 (xyz) (xyz) (xyz)))))
+
+;;(defparameter *faces* 0)
+(defun getmeshersfinishedshit ()
+  (multiple-value-bind (buf len coords iter) (sb-thread:join-thread mesher-thread)
+    ;;   (incf *faces* len)
+    (when coords
+      (let ((old-call-list (get-chunk-display-list coords)))
+					;	(print len)
+	(aplayground::reset-attrib-buffer-iterators iter)
+	(let ((new (let ((list (gl:gen-lists 1)))
+		     (gl:new-list list :compile)
+		     (gl:with-primitives :quads
+		       (mesh-chunk len buf))
+		     (gl:end-list)
+		     list)))
+	  (if new
+	      (set-chunk-display-list coords new)
+	      (remove-chunk-display-list coords)))
+	(when old-call-list (gl:delete-lists old-call-list 1))
+	(let ((old-world (get-display-list :world)))
+	  (remove-display-list :world)
+	  (when old-world (gl:delete-lists old-world 1))))))
+  (setf mesher-thread nil))
+
+(defun mesherthreadbusy ()
+  (not (or (eq nil mesher-thread)
+	   (not (bordeaux-threads:thread-alive-p mesher-thread)))))
+
+(defun giveworktomesherThread (thechunk)
+  (setf mesher-thread
+	(bordeaux-threads:make-thread
+	 (lambda ()
+	   (let ((iter aplayground::*attrib-buffer-iterators*))
+	     (aplayground::reset-attrib-buffer-iterators iter)
+	     (sb-thread:return-from-thread
+	      (chunk-shape thechunk iter)))))))
+#+nil
+(defun getmeshersfinishedshit ()
+  (multiple-value-bind (shape len coords) (sb-thread:join-thread mesher-thread)
+    ;;   (incf *faces* len)
+    (when coords
+      (when shape
+	(let ((old-call-list (get-chunk-display-list coords)))
+	  (let ((new (shape-list shape len)))
+	    (if new
+		(set-chunk-display-list coords new)
+		(remove-chunk-display-list coords)))
+	  (when old-call-list (gl:delete-lists old-call-list 1))
+	  (let ((old-world (get-display-list :world)))
+	    (remove-display-list :world)
+	    (when old-world (gl:delete-lists old-world 1)))))))
+  (setf mesher-thread nil))
+
+#+nil
 (defun shape-list (verts len)   
   (declare (type fixnum len)
 	   (type (simple-array single-float *) verts))
@@ -54,30 +114,3 @@
 		 (RETURN-FROM NIL (PROGN NIL)))))))
       (gl:end-list)
       ourlist)))
-;;(defparameter *faces* 0)
-(defun getmeshersfinishedshit ()
-  (multiple-value-bind (shape len coords) (sb-thread:join-thread mesher-thread)
- ;;   (incf *faces* len)
-    (when coords
-      (when shape
-	(let ((old-call-list (get-chunk-display-list coords)))
-	  (let ((new (shape-list shape len)))
-	    (if new
-		(set-chunk-display-list coords new)
-		(remove-chunk-display-list coords)))
-	  (when old-call-list (gl:delete-lists old-call-list 1))
-	  (let ((old-world (get-display-list :world)))
-	    (remove-display-list :world)
-	    (when old-world (gl:delete-lists old-world 1)))))))
-  (setf mesher-thread nil))
-
-(defun mesherthreadbusy ()
-  (not (or (eq nil mesher-thread)
-	   (not (bordeaux-threads:thread-alive-p mesher-thread)))))
-
-(defun giveworktomesherThread (thechunk)
-  (setf mesher-thread
-	(bordeaux-threads:make-thread
-	 (lambda ()
-	   (sb-thread:return-from-thread
-	    (chunk-shape thechunk))))))
