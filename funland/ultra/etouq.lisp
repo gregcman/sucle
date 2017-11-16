432e83d404c3b7bc84a2934e180b4dc82d5ee5ac
terminal625 <zpersoncool@gmail.com>
2017-11-16 17:05:55 -0500

    etouq wrong

--- funland/ultra/etouq.lisp
+++ funland/ultra/etouq.lisp
@@ -1,5 +1,11 @@
 (in-package :fuktard)
 
 (export (quote etouq))
+#+nil
 (defmacro etouq (form)
   (eval form))
+
+(defmacro etouq (form)
+  (let ((var (gensym)))
+    `(macrolet ((,var () ,form))
+       (,var))))
