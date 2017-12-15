3af8ac84168e8635309e6406bcbc48cb71ed2112
terminal625 <zpersoncool@gmail.com>
2017-12-14 21:00:47 -0500

    lazy loading real minecraft worlds?

--- game/funfair/test.lisp
+++ game/funfair/test.lisp
@@ -366,6 +366,13 @@
 		      (funcall fun a c b))))
 	   world::chunkhash))
 
+(defun map-all-chunks1 (fun)
+  (maphash (lambda (k v)
+	     (declare (ignore v))
+	     (multiple-value-bind (x y z) (world:unhashfunc k)
+	       (funcall fun x y z)))
+	   world::chunkhash))
+
 (defun tree (x y z)
   (let ((trunk-height (+ 1 (random 3))))
     (let ((yup (+ y trunk-height)))
@@ -392,3 +399,125 @@
 	       (sandbox::plain-setblock (+ x x0) (+ yup y0) (+ z z0) 18 0 0))))
     (dobox ((y0 y (+ y (+ 3 trunk-height))))
 	   (sandbox::plain-setblock x y0 z 17 0 0))))
+
+#+nil
+(progno
+ (in-package :sandbox)
+
+ (defun setatest (x)
+   (prog2 (setf atest
+		(case x
+		  (0 (byte-read #P "/home/terminal256/.minecraft/saves/New World-/region/r.0.-1.mcr"))
+		  (1 (byte-read #P "/home/imac/.minecraft/saves/New World/region/r.1.1.mcr"))
+		  (2 (cl-mc-shit::testchunk))
+		  (3 (byte-read #P "/home/imac/info/mcp/jars/saves/New World/region/r.-1.-1.mcr"))
+		  ))
+       x))
+
+ (defparameter atest nil)
+ ;;(setatest 3)
+
+ (defun someseq (x y)
+   (let* ((thechunk (helpchunk (+ 24 x) y)))
+     (if thechunk
+	 (let ((light (getlightlizz thechunk))
+	       (blocks (getblockslizz thechunk))
+	       (skylight (getskylightlizz thechunk))
+	       (meta (getmetadatalizz thechunk))
+					;      (leheight (getheightlizz thechunk))
+	       )
+	   (let ((xscaled (ash x 4))
+		 (yscaled (ash y 4)))
+	     (progn (sandbox::flat3-chunk
+		     light
+		     (lambda (x y z b)
+		       (setf (world:getlight x y z) b))
+		     xscaled 0 yscaled)
+		    (sandbox::flat3-chunk
+		     skylight
+		     (lambda (x y z b)
+		       (setf (world:skygetlight x y z) b))
+		     xscaled 0 yscaled)
+		    (sandbox::flat3-chunk
+		     meta
+		     (lambda (x y z b)
+		       (setf (world:getmeta x y z) b))
+		     xscaled 0 yscaled)
+		    (progno (sandbox::flat2-chunk
+			     leheight
+			     (lambda (x y b)
+			       (setf (world::getheight x y) b))
+			     xscaled yscaled)))
+	     (sandbox::flat3-chunk
+	      blocks
+	      (lambda (x y z b)
+		(unless (zerop b)
+		  (setf  (world:getblock x y z) b)))
+	      xscaled 0 yscaled))))))
+
+ (defun flat3-chunk (data setfunc xoffset yoffset zoffset)
+   (dotimes (wow 8)
+     (dotimes (j 16)
+       (dotimes (i 16)
+	 (dotimes (k 16)
+	   (funcall setfunc (+ xoffset i) (+ yoffset (* 16 wow) j) (+ zoffset k)
+		    (elt data (+ (* i 16 128) (+ j (* 16 wow)) (* k 128)))))))))
+
+ (defun flat2-chunk (data setfunc xoffset yoffset)
+   (dotimes (j 16)
+     (dotimes (i 16)
+       (funcall setfunc (+ xoffset i) (+ yoffset j)
+		(elt data (+ i (+ (* 16 j))))))))
+
+ (defun helpchunk (x y)
+   (let ((thechunk  (cl-mc-shit:mcr-chunk atest x y)))
+     (if thechunk
+	 (cl-mc-shit:chunk-data
+	  thechunk)
+	 nil)))
+
+ (defun expand-nibbles (vec)
+   (let* ((len (length vec))
+	  (newvec (make-array (* 2 len) :element-type '(unsigned-byte 8))))
+     (dotimes (x len)
+       (multiple-value-bind (a b) (floor (aref vec x) 16)
+	 (setf (aref newvec (* 2 x)) b)
+	 (setf (aref newvec (1+ (* 2 x))) a)))
+     newvec))
+
+ (defun nbt-open (lizz)
+   (third
+    (first
+     (third
+      lizz))))
+
+ (defun gettag (lestring lizz)
+   (dolist (tag lizz)
+     (if (equal lestring (second tag))
+	 (return-from gettag (third tag)))))
+
+ (defun getmetadatalizz (lizz)
+   (expand-nibbles
+    (gettag "Data"
+	    (nbt-open lizz))))
+
+ (defun getskylightlizz (lizz)
+   (expand-nibbles
+    (gettag "SkyLight"
+	    (nbt-open lizz))))
+
+
+ (defun getlightlizz (lizz)
+   (expand-nibbles
+    (gettag "BlockLight"
+	    (nbt-open lizz))) )
+ (defun getblockslizz (lizz)
+   (gettag
+    "Blocks"
+    (nbt-open lizz)))
+
+ (defun getheightlizz (lizz)
+   (gettag
+    "HeightMap"
+    (nbt-open lizz)))
+ )
