a101b8112af0f53ddcdc6bf924a0d596c7928daf
terminal625 <zpersoncool@gmail.com>
2018-01-08 17:20:19 -0500

    more music cleanup
    music cleanup for the loss

--- game/funfair/music.asd
+++ game/funfair/music.asd
@@ -1,7 +1,8 @@
 (asdf:defsystem #:music
   :depends-on (:cl-openal
 	       :cl-alc
-	       :cl-ffmpeg)
+	       :cl-ffmpeg
+	       :bordeaux-threads)
     :serial t
     :components 
     ((:file "openal")))
--- game/funfair/openal.lisp
+++ game/funfair/openal.lisp
@@ -10,84 +10,162 @@
    ))
 (in-package #:sound-stuff)
 
-(defparameter *esc* t)
+
+(defmacro subprocess ((&rest vars) &body body)
+  (let ((syms (mapcar (lambda (x) (gensym (string x))) vars)))
+    `(bordeaux-threads:make-thread
+      (let ,(mapcar #'list syms vars)
+	(lambda ()
+	  (let ,(mapcar #'list vars syms)
+	    ,@body))))))
+(defmacro iosub (&body body)
+  `(subprocess (*standard-output* *standard-input* *terminal-io*)
+     ,@body))
+
+#+nil
+(progno     
+     (al:listener :position vec)
+     (let ((curr *velocity*))
+       (setf (aref curr 0) *xvel*)
+       (setf (aref curr 1) *yvel*)
+       (setf (aref curr 2) *zvel*)
+       (al:listener :velocity curr))
+     (let ((curr *orientation*)
+	   (other (camera-vec-forward *camera*))
+	   (other2 (camera-vec-up *camera*)))
+       (setf (aref curr 0) (- (aref other 0)))
+       (setf (aref curr 1) (- (aref other 1)))
+       (setf (aref curr 2) (- (aref other 2)))
+       (setf (aref curr 3) (aref other2 0))
+       (setf (aref curr 4) (aref other2 1))
+       (setf (aref curr 5) (aref other2 2))
+       (al:listener :orientation curr)))
+
 (defparameter *data* nil)
-(defparameter *playback* (or :mono8
-			     :mono16
-			     :stereo8
-			     :stereo16))
+(defparameter *playback* (or; :mono8
+			     ;:mono16
+			     ;:stereo8
+			     :stereo16
+			     ))
+(defparameter *source* nil)
+(defparameter *time-remaining* 0.0)
+(defparameter *used-buffers* (make-hash-table :test 'eql))
+;;do not switch source formats!!!!
+
+(defparameter *esc* t)
+(defun getpacket (data samples channels audio-format rate)
+  (let ((format *playback*))
+    (multiple-value-bind (array playsize)
+	(sound-stuff::convert
+	 (case channels
+	   (1 (cffi:mem-aref data :pointer 0))
+	   (otherwise (cffi:mem-aref data :pointer 1)))
+	 (cffi:mem-aref data :pointer 0)
+	 samples
+	 audio-format
+	 format)
+      (sound-stuff::playmem format array playsize rate)
+      (cffi::foreign-free array)))
+  nil)
 (defun alut-test (music)
-  (setf *esc* nil)
+  (setf *esc* nil
+	*time-remaining* 0.0)
   (let ((music (cl-ffmpeg::init-music-stuff music)))
     (tagbody rep
-       (let ((more?
-	      (cl-ffmpeg::get-sound-buff
-	       music
-	       (lambda (data samples channels audio-format rate)
-		 (multiple-value-bind (array playsize)
-		     (sound-stuff::convert
-		      (case channels
-			(1 (cffi:mem-aref data :pointer 0))
-			(otherwise (cffi:mem-aref data :pointer 1)))
-		      (cffi:mem-aref data :pointer 0)
-		      samples
-		      audio-format
-		      sound-stuff::*playback*)
-		   (sound-stuff::playmem array playsize rate)
-		   (cffi::foreign-free array))
-		 nil))))
-	 (when more?
-	   (loop
-	      (progn
-		(when *esc* (return))
-		(free-buffers)
-		(when (< *time-remaining* 0.5)
-		  (go rep)))))))
+       (when (cl-ffmpeg::get-sound-buff music #'getpacket)
+	 (loop
+	    (progn
+	      (when *esc* (return))
+	      (free-buffers)
+	      (let ((almost 0.5))
+		(if (<= *time-remaining* almost)
+		    (go rep)
+		    (sleep (max 0.0 (- *time-remaining* almost)))
+		    ))))))
     (cl-ffmpeg::free-music-stuff music)
     (print "data dumped: alut-test ")
     music))
 
+(defun reset-listener ()
+  (al:listener :gain 1.0)
+  (al:listener :position (vector 0.0 0.0 0.0))
+  (al:listener :orientation (vector 0.0 1.0 0.0 0.0 1.0 0.0)))
+(defun alut-hello-world (&optional (source *source*))
+  (al:source source :position (vector 0.0 0.0 0.0))
+  (al:source source :velocity (vector 0.0 0.0 0.0))
+  (al:source source :gain 1.0)
+  (al:source source :pitch 1.0))
 (defun load-file (file)
+  (when *source* (al:delete-source *source*))
+  (setf *source* (al:gen-source))
   (alut-hello-world)
   (setf *data* (alut-test file)))
 (defun play ()
   (al:source-play *source*))
-(defparameter *time-remaining* 0.0)
 (defun pause ()
   (al:source-pause *source*))
 (defun stop ()
   (setf *esc* t)
   (al:source-stop *source*)
   (free-buffers)
+  (free-buffers-hash *used-buffers*)
+  (clrhash *used-buffers*)
   (setf *time-remaining* 0.0))
-(defparameter *free-buffers* nil)
-
-(defun get-buffer ()
-					; (print "get-buffer")
-  (free-buffers)
-  (or (pop *free-buffers*)
-      (al:gen-buffer)))
-
 (defun source-unqueue-buffer (sid)
-  (cffi:with-foreign-object (buffer-array :uint)
-    (setf (cffi:mem-ref buffer-array :uint) 0)
-    (%al:source-unqueue-buffers sid 1 buffer-array)
-    (unless (zerop (cffi:mem-ref buffer-array :uint))
-      (cffi:mem-aref buffer-array :uint 0))))
-(defun free-buffers ()
-  (let ((bufs (al:get-source *source* :buffers-processed)))
-    (unless (zerop bufs)
-      (loop
-	 repeat bufs do
-	   (let ((buf (source-unqueue-buffer *source*)))
-	     (when buf
-	       (decf *time-remaining* (buffer-seconds buf))
-	       (when buf
-		 (push buf *free-buffers*))))))
-    bufs))
-
+  (let ((value (%source-unqueue-buffer sid)))
+    (when value
+      (remhash value *used-buffers*))
+    value))
+(defun playmem (format pcm playsize rate)
+  (let ((buffer (get-buffer)))
+    (al:buffer-data buffer format pcm playsize rate)
+    (source-queue-buffer *source* buffer)))
 (defun source-queue-buffer (sid buffer)
   (incf *time-remaining* (buffer-seconds buffer))
+  (setf (gethash buffer *used-buffers*) t)
+  (%source-queue-buffer sid buffer))
+(defun free-buffers ()
+  (multiple-value-bind (time bufs)
+      (%free-buffers *source*)
+    (decf *time-remaining* time)
+    (values time bufs)))
+
+(defun free-buffers-hash (hash)
+  (bordeaux-threads:with-lock-held (*free-buffers-lock*)
+    (funland::dohash (k v) hash
+      (declare (ignore v))
+      (push k *free-buffers*))))
+
+(defun get-buffer ()
+  (or (bordeaux-threads:with-lock-held (*free-buffers-lock*)
+	(pop *free-buffers*))
+      (al:gen-buffer)))
+(defparameter *free-buffers* nil)
+(defparameter *free-buffers-lock* (bordeaux-threads:make-lock "free albuffers"))
+(defun %free-buffers (sid)
+  (let ((bufs (al:get-source sid :buffers-processed))
+	(time 0.0))
+    (when (< 0 bufs)
+      (dotimes (i bufs)
+	(let ((buf (source-unqueue-buffer sid)))
+	  (when buf
+	    (incf time (buffer-seconds buf))
+	    (bordeaux-threads:with-lock-held (*free-buffers-lock*)
+	      (push buf *free-buffers*))))))
+    (values
+     time
+     bufs)))
+
+(defun %source-unqueue-buffer (sid)
+  (cffi:with-foreign-object (buffer-array :uint 1)
+    (setf (cffi:mem-aref buffer-array :uint 0) 0)
+    (%al:source-unqueue-buffers sid 1 buffer-array)
+    (let ((item (cffi:mem-aref buffer-array :uint 0)))
+      (if (zerop item)
+	  nil
+	  item))))
+
+(defun %source-queue-buffer (sid buffer)
   (let ((empty? (al:get-source sid :buffers-queued)))
     (cffi:with-foreign-object (buffer-array :uint 1)
       (setf (cffi:mem-aref buffer-array :uint 0)
@@ -96,6 +174,8 @@
     (when (zerop empty?)
       (unless (eq :paused (al:get-source sid :source-state))
 	(al:source-play sid)))))
+
+;;;;;
 
 (defmacro floatify (x)
   `(coerce ,x 'single-float))
@@ -107,26 +187,7 @@
     (/ (/ (* size 8) channels bits)
        frequency)))
 
-(defun playmem (pcm playsize rate)
-  (free-buffers)
-  (let ((buffer (get-buffer)))
-    (al:buffer-data buffer *playback* pcm playsize rate
-		    )
-    (source-queue-buffer *source* buffer)))
-
-(defun alut-hello-world ()
-  (alc:make-context-current *alc-context*)
-  (let ((source *source*))
-    (al:source source :position (vector 0.0 0.0 0.0))
-    (al:source source :velocity (vector 0.0 0.0 0.0))
-    (al:source source :gain 1.0)
-    (al:source source :pitch 1.0)
-    (al:listener :gain 1.0)
-    (al:listener :position (vector 0.0 0.0 0.0))
-    (al:listener :orientation (vector 0.0 1.0 0.0 0.0 1.0 0.0))))
-
-(defparameter *source* nil)
-
+;;;;initialization
 (defparameter *alc-device* nil)
 (defun open-device ()
   (close-device)
@@ -147,49 +208,26 @@
       (alc:make-context-current (cffi:null-pointer)))
     (alc:destroy-context *alc-context*))
   (setf *alc-context* nil))
-
 (defun start-al ()
   (open-device)
   (open-context)
-  (setf *source* (al:gen-source)))
-
+  (alc:make-context-current *alc-context*)
+  (reset-listener))
 (defun destroy-al ()
   (close-context)
-  (close-device))
-
-
+  (close-device)
+  (setf *al-on?* nil))
 (defparameter *al-on?* nil)
-(unless *al-on?*
-  (start-al)
-  (setf *al-on?* t))
+(defun really-start ()
+  (unless *al-on?*
+    (start-al)
+    (setf *al-on?* t)))
+(eval-when (:execute :load-toplevel)
+  (really-start))
 
-(progno
-     ;;  (alc:make-context-current fuck::*alc-context*)
-     
-     (al:listener :position vec)
-     (let ((curr *velocity*))
-       (setf (aref curr 0) *xvel*)
-       (setf (aref curr 1) *yvel*)
-       (setf (aref curr 2) *zvel*)
-       (al:listener :velocity curr))
-     (let ((curr *orientation*)
-	   (other (camera-vec-forward *camera*))
-	   (other2 (camera-vec-up *camera*)))
-       (setf (aref curr 0) (- (aref other 0)))
-       (setf (aref curr 1) (- (aref other 1)))
-       (setf (aref curr 2) (- (aref other 2)))
-       (setf (aref curr 3) (aref other2 0))
-       (setf (aref curr 4) (aref other2 1))
-       (setf (aref curr 5) (aref other2 2))
-       (al:listener :orientation curr)))
 
-#+nil
-(when (window:key-j-p :u)
-  (al:source *source* :position (list sandbox::*xpos*
-				      sandbox::*ypos*
-				      sandbox::*zpos*))
-  (alut-hello-world))
 
+;;;;ffmpeg format to openal format
 (defun convert (left right len format playblack-format)
   (let* ((arrcount (case playblack-format
 		     ((:stereo8 :stereo16) (* len 2))
@@ -205,16 +243,19 @@
        (values (convert16 left len format arr)
 	       (* 2 len)))
       (:stereo8
-       (values (interleave8 left right len format (* 2 len) arr)
+       (values (interleave8 left right format (* 2 len) arr)
 	       (* len 2)))
       (:stereo16
-       (values (interleave16 left right len format (* 2 len) arr)
+       (values (interleave16 left right format (* 2 len) arr)
 	       (* len 4))))))
 
 (defmacro clamp (min max x)
   `(max ,min (min ,max ,x)))
 
 
+;;	DC DAC Modeled -> [-1.0 1.0] -> [-32768 32767]
+;;      apple core audo, alsa, matlab, sndlib -> (lambda (x) (* x #x8000))
+;;
 ;;;;crackling noise when floats above 1.0 or below -1.0?
 ;;;; clamp or scale floats outside of [-1.0 1.0]?
 (eval-when (:compile-toplevel)
@@ -222,24 +263,28 @@
     '(case format
       ((:fltp :flt)
        (let* ((scale 1.0)
-	      (scaling-factor (/ 32767.5 scale)))
+	      (scaling-factor (/ 32768.0 scale)))
 	 (declare (type single-float scale scaling-factor))
 	 (audio-type :float
-		     (round
-		      (- (* (clamp -1.0 1.0 value)
-			    scaling-factor)
-			 0.5)))))
+		     (clamp
+		      #x-8000 #x7fff
+		      (the fixnum
+			   (round
+			    (* value
+			       scaling-factor)))))))
+      
       ((:dblp :dbl)
        (let* ((scale 1.0d0)
-	      (scaling-factor (/ 32767.5d0 scale)))
+	      (scaling-factor (/ 32768.0d0 scale)))
 	 (declare (type double-float scale scaling-factor))
 	 (audio-type :double
-		     (round
-		      (- 
-			 (* 
-			  (clamp -1.0d0 1.0d0 value)
-			  scaling-factor)
-			 0.5d0)))))
+		     (clamp
+		      #x-8000 #x7fff
+		      (the fixnum
+			   (round
+			    (* 
+			     value
+			     scaling-factor)))))))
       ((:s16 :s16p)
        (audio-type :int16 value))
       ((:s32 :s32p)
@@ -250,10 +295,12 @@
        (audio-type :int64 (ash (the (signed-byte 64) value) -48)))
       (:nb (error "wtf is nb?")))))
 
+(deftype carray-index ()
+  `(integer 0 ,(load-time-value (ash most-positive-fixnum -3))))
 ;;;length -> samples per channel
-(defun interleave16 (left right length format numcount arr)
+(defun interleave16 (left right format numcount arr)
   (declare (optimize (speed 3) (safety 0)))
-  (declare (type fixnum length numcount))
+  (declare (type carray-index numcount))
   (macrolet ((audio-type (type form)
 	       `(dotimes (index numcount)
 		  (setf (cffi:mem-aref arr :int16 index)
@@ -267,7 +314,7 @@
 
 (defun convert16 (buffer newlen format arr)
   (declare (optimize (speed 3) (safety 0)))
-  (declare (type fixnum newlen))
+  (declare (type carray-index newlen))
   (macrolet ((audio-type (type form)
 	       `(dotimes (index newlen)
 		  (setf (cffi:mem-aref arr :int16 index)
@@ -281,26 +328,27 @@
     '(case format
       ((:fltp :flt)
        (let* ((scale 1.0)
-	      (scaling-factor (/ 127.5 scale)))
+	      (scaling-factor (/ 128.0 scale)))
 	 (declare (type single-float scale scaling-factor))
 	 (audio-type :float
-		     (round
-		      (- 
-			 (* 
-			  (clamp -1.0 1.0 value)
-			  scaling-factor)
-			 0.5)))))
+		     (clamp
+		      -128 127
+		      (the fixnum
+			   (round
+			    (* value
+			       scaling-factor)))))))
       ((:dblp :dbl)
        (let* ((scale 1.0d0)
-	      (scaling-factor (/ 127.5d0 scale)))
+	      (scaling-factor (/ 128d0 scale)))
 	 (declare (type double-float scale scaling-factor))
 	 (audio-type :double
-		     (round
-		      (- 
-			 (* 
-			  (clamp -1.0d0 1.0d0 value)
-			  scaling-factor)
-			 0.5d0)))))
+		     (clamp
+		      -128 127
+		      (the fixnum
+			   (round
+			    (*
+			     value
+			     scaling-factor)))))))
       ((:s16 :s16p)
        (audio-type :int16 (+ 128 (ash value -8))))
       ((:s32 :s32p)
@@ -308,12 +356,13 @@
       ((:u8 :u8p)
        (audio-type :uint8 value))
       ((:s64 :s64p)
-       (audio-type :int64 (+ 128 (ash (the (signed-byte 64) value) -56))))
+       (audio-type :int64
+	(+ 128 (ash (the (unsigned-byte 64) value) -56))))
       (:nb (error "wtf is nb?")))))
 
-(defun interleave8 (left right length format numcount arr)
+(defun interleave8 (left right format numcount arr)
   (declare (optimize (speed 3) (safety 0)))
-  (declare (type fixnum length numcount))
+  (declare (type carray-index numcount))
   (macrolet ((audio-type (type form)
 	       `(dotimes (index numcount)
 		  (setf (cffi:mem-aref arr :uint8 index)
@@ -327,7 +376,7 @@
 
 (defun convert8 (buffer newlen format arr)
   (declare (optimize (speed 3) (safety 0)))
-  (declare (type fixnum newlen))
+  (declare (type carray-index newlen))
   (macrolet ((audio-type (type form)
 	       `(dotimes (index newlen)
 		  (setf (cffi:mem-aref arr :uint8 index)
@@ -335,3 +384,46 @@
 			  ,form)))))
     (etouq *uint8-dispatch*))
   arr)
+
+
+#|
+
+	Int to Float
+	Float to Int*
+	Transparency
+	Used By
+0)
+	((integer + .5)/(0x7FFF+.5)
+	float*(0x7FFF+.5)-.5
+	Up to at least 24-bit
+	DC DAC Modeled
+1)
+	(integer / 0x8000)
+	float * 0x8000
+	Up to at least 24-bit
+	Apple (Core Audio)1, ALSA2, MatLab2, sndlib2
+2)
+	(integer / 0x7FFF)
+	float * 0x7FFF
+	Up to at least 24-bit
+	Pulse Audio2
+3)
+	(integer / 0x8000)
+	float * 0x7FFF
+	Non-transparent
+	PortAudio1,2, Jack2, libsndfile1,3
+4)
+	(integer>0?integer/0x7FFF:integer/0x8000)
+	float>0?float*0x7FFF:float*0x8000
+	Up to at least 24-bit
+	At least one high end DSP and A/D/A manufacturer.2,4 XO Wave 1.0.3.
+5)
+	Uknown
+	float*(0x7FFF+.49999)
+	Unknown
+	ASIO2
+*obviously, rounding or dithering may be required here.
+Note that in the case of IO APIs, drivers are often responsible for conversions. The conversions listed here are provided by the API.
+
+from:: http://blog.bjornroche.com/2009/12/int-float-int-its-jungle-out-there.html
+|#
