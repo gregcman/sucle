8f2d1417c78d9e1974d7290b0b67a6c16e2d9d08
terminal625 <zpersoncool@gmail.com>
2019-01-02 04:03:02 -0500

    update struct to clos to include conc-names

--- src/uncommon-lisp/struct-to-clos.lisp
+++ src/uncommon-lisp/struct-to-clos.lisp
@@ -10,12 +10,25 @@
 (defmacro struct->class (form)
   (%struct->class form))
 
+;;;;see http://www.lispworks.com/documentation/lw51/CLHS/Body/m_defstr.htm
+(defun get-conc-name (name-and-options)
+  (cond ((find :conc-name name-and-options) "")
+	(t (let ((cell (find-if (lambda (x) (eq :conc-name (first x)))
+				name-and-options)))
+	     (if cell
+		 (let ((name (second cell)))
+		   (if name
+		       name
+		       ""))
+		 nil)))))
+
 (defun %struct->class (form)
   (destructuring-bind (header title &rest slots) form
     (declare (ignore header))
     (unless (symbolp title)
       (setf title (first title)))
-    (let ((conc-name (conc-name title))
+    (let ((conc-name (or (get-conc-name title)
+			 (conc-name title)))
 	  (new-slots ()))
       (flet ((add-slot (name &optional value)
 	       (push `(,name :initform ,value
