e599621f13f732ff5b0389c24798aae56c8650ba
terminal625 <zpersoncool@gmail.com>
2019-04-28 01:32:23 -0400

    remove the "triangles" asdf system

--- application/testbed/sandbox-subsystem.lisp
+++ application/testbed/sandbox-subsystem.lisp
@@ -673,9 +673,40 @@
 
 (defvar *ourdir* (asdf:system-source-directory :testbed))
 
+(defun barycentric-interpolation (px py vx1 vy1 vx2 vy2 vx3 vy3)
+  (let ((denominator (+ (*
+			 (- vy2 vy3)
+			 (- vx1 vx3))
+			(*
+			 (- vx3 vx2)
+			 (- vy1 vy3))))
+	(py-yv3 (- py vy3))
+	(px-xv3 (- px vx3)))
+    (let* ((w1 (/
+		(+
+		 (*
+		  (- vy2 vy3)
+		  px-xv3)
+		 (*
+		  (- vx3 vx2)
+		  py-yv3))
+		  denominator))
+	   (w2 (/
+		(+
+		 (*
+		  (- vy3 vy1)
+		  px-xv3)
+		 (*
+		  (- vx1 vx3)
+		  py-yv3))
+		denominator))
+	   (w3 (- 1 w1 w2)))
+      (values w1 w2 w3))))
+
+
 (defun  foliage-color (a b)
   (multiple-value-bind (w1 w2 w3)
-      (triangles:barycentric-interpolation a b 0.0 0.0 255.0 0.0 255.0 255.0)
+      (barycentric-interpolation a b 0.0 0.0 255.0 0.0 255.0 255.0)
     (mapcar (lambda (x y z)
 	      (+ (* x w1)
 		 (* y w2)
--- src/euclidean-geometry/triangles/triangles.asd
+++
@@ -1,7 +0,0 @@
-(asdf:defsystem #:triangles
-  :author "terminal625"
-  :license "MIT"
-  :description "Triangles and barycentric interpolation"
-  :depends-on ()
-  :components 
-  ((:file "triangles"))) 
