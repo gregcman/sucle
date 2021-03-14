21e878f1d48ade13f848649c0db43bb60d1eb98d
gregcman <terminal625@gmail.com>
2021-03-14 11:46:56 -0400

    bert

--- src/minisucle/extra/hug/hug.lisp
+++ src/minisucle/extra/hug/hug.lisp
@@ -16,14 +16,25 @@ model = BertForMaskedLM.from_pretrained('bert-base-cased')
     (&optional (str "[MASK] [MASK] [MASK] of the United States mismanagement of the Coronavirus is its distrust of science."))
   (setf (py4cl2:pyeval "input_txt")
 	(py4cl2:pythonize str))
+  (setf (py4cl2:pyeval "top") 10)
   (py4cl2:pyexec
    "
 inputs = tokenizer(input_txt, return_tensors='pt')
+
+# 
+len = list(inputs.input_ids.size())[1]
+# print(len)
 outputs = model(**inputs)
 predictions = outputs[0]
 sorted_preds, sorted_idx = predictions[0].sort(dim=-1, descending=True)
-for k in range(10):
-   predicted_index = [sorted_idx[i, k].item() for i in range(0,24)]
-   predicted_token = [tokenizer.convert_ids_to_tokens([predicted_index[x]])[0] for x in range(1,24)]
-   print(predicted_token)"
-   ))
+
+acc = []
+for k in range(top):
+   predicted_index = [sorted_idx[i, k].item() for i in range(0,len)]
+   predicted_token = [tokenizer.convert_ids_to_tokens([predicted_index[x]])[0] for x in range(1,len)]
+   acc.append(predicted_token)"
+   )
+
+  (py4cl2:pyeval "acc"))
+
+;;https://stackoverflow.com/questions/46826218/pytorch-how-to-get-the-shape-of-a-tensor-as-a-list-of-int
